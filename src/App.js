fbf198354c5f858cdf1fafa21bd97324c1d52934
gheryesss <gheryesss@gmail.com>
2021-03-21 23:01:05 +0300

    Init project and create structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import {  Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
+import HomePage from './pages/home/home.component';
+import CatalogPage from './pages/catalog/catalog.component';
+import Header from './components/header/header.component';
+
+import db from './firebase/firebase.utils';
+
 function App() {
+  db.collection("categories/smartphones/products").get().then((querySnapshot) => {
+    console.log(querySnapshot.docs.length);
+    querySnapshot.forEach(async (doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/catalog' component={CatalogPage} />
+      </Switch>
+    </>
   );
 }
 
